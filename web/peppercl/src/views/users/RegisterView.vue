2c53886f921387f21e24f5ae6ce88bcc6671e09d
skoros <daniel.skoros@gmail.com>
2023-10-22 12:30:45 +0200

    usersview

---
+++ web/peppercl/src/views/users/RegisterView.vue
@@ -0,0 +1,21 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const formFields = reactive({
+        
+    })
+    return {formFields}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    
+</style>

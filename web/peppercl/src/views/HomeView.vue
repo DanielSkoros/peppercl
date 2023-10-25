6c6c01903ea565d40baa45084d0d8edb61f162bb
skoros <daniel.skoros@gmail.com>
2023-10-25 18:51:21 +0200

    colors

--- web/peppercl/src/views/HomeView.vue
+++ web/peppercl/src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <template>
-    content dd
+  <div class="color__wrapper">
+    <div v-for="color in colors" :class="color" class="color-box" :key="color"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
-
+const colors = ['green', 'sea', 'light-brown', 'brown', 'dark-gray', 'light-gray']
 </script>
+
+<style scoped lang="scss">
+.color__wrapper {
+  width: 100%;
+  display: flex;
+  height: 100vh;
+}
+.color-box {
+  width: 100%;
+  height: 100vh;
+}
+</style>

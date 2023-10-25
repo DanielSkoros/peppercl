a0590860bf9f1f6444d42348a5ad26e6fbfdc85e
skoros <daniel.skoros@gmail.com>
2023-10-25 19:40:23 +0200

    init borger

--- web/peppercl/src/components/nav/NavBar.vue
+++ web/peppercl/src/components/nav/NavBar.vue
@@ -1,41 +1,32 @@
 <template>
-    <nav class="nav-container">
-        <div class="nav__logo">
-            ddddd
-        </div> 
-        <div @click="setIsOpen" class="nav-burger__icon">
-            <div class="nav-burger__bar"></div>
-            <div class="nav-burger__bar"></div>
-            <div class="nav-burger__bar"></div>
-        </div> 
-    </nav>
+  <nav class="nav-container relative">
+    <div :class="{'move-out': sidebarOpen}" class="back-arrow animated">&lt;</div>
+    <div class="nav__logo">LOGO</div>
+    <SideBar @open="(state) => (sidebarOpen = state)" />
+  </nav>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
-
-const isOpen = ref<Boolean>(false)
-const setIsOpen = () => {isOpen.value = !isOpen.value}
-
+import SideBar from './SideBar.vue'
+const sidebarOpen = ref<Boolean>(false)
 </script>
 
-<style lang="scss" scoped>
-    .nav-container {
-        color: white;
-        width: 100%;
-        background: black;
-        display: flex;
-        justify-content: between;
-        .nav-burger__icon {
+<style lang="scss">
+.nav-container {
+  color: white;
+  width: 100%;
+  background: black;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 10px;
 
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-        .nav-burger__bar {
-            height: 5px;
-            width: 40px;
-            background-color: white;
-        }
+  .back-arrow {
+    transition-delay: 250ms;
+    transition-duration: 0.5s;
+    &.move-out {
+        transition-duration: 2s !important;
     }
-    }
+  }
+}
 </style>
---
+++ web/peppercl/src/components/nav/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sidebar__wrapper relative">
+    <div @click.stop="setIsOpen" class="nav-burger__icon" :class="{ open: isOpen }">
+      <div class="nav-burger__bar" :class="{ open: isOpen }"></div>
+      <div class="nav-burger__bar" v-show="!isOpen"></div>
+      <div class="nav-burger__bar" :class="{ open__reverse: isOpen }"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const emit = defineEmits(['open'])
+
+const isOpen = ref<Boolean>(false)
+const setIsOpen = () => {
+  isOpen.value = !isOpen.value
+  emit('open', isOpen.value)
+}
+</script>
+
+<style lang="scss" scoped>
+.nav-burger__icon {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  transition: transform 0.5s ease;
+
+  &.open {
+    transform: translateX(calc(-100vw + 45px)) translateY(6px);
+  }
+  .nav-burger__bar {
+    height: 3px;
+    width: 25px;
+    background-color: white;
+    transition: transform 0.5s ease;
+
+    &.open {
+      transform: rotate(45deg) translateY(6px);
+    }
+    &.open__reverse {
+      transform: rotate(-45deg) translateY(-6px);
+    }
+  }
+}
+</style>

9f9974d59d242cb3a01f8305ccebbcf250f4ea62
skoros <daniel.skoros@gmail.com>
2023-10-27 16:42:20 +0200

    ts node

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,27 +1,27 @@
+import { IUser } from "../db/models/user";
+
 var express = require('express');
 var router = express.Router();
-
-const { MongoModel } = require('../db/generic.ts')
 
 const { UserModel, userSchema } = require('../db/models/user.ts')
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  const model = new MongoModel('User', userSchema);
+router.get('/', async function(req: any, res: { send: (arg0: any) => void; }, next: any) {
+  const model = new UserModel('User', userSchema);
   
   const users = await model.all()
   res.send(users);
 });
 
-router.post('/', async function(req, res) {
+router.post('/', async function(req: { json: (arg0: any) => any; body: any; }, res: { send: (arg0: string) => void; }) {
   const userBody = req.json(req.body);
   const user = new UserModel(userBody)
   await user.save()
   res.send('respond with a resource');
 });
 
-router.get('/:id', async (req, res) => {
-  const model = new MongoModel('User', userSchema);
+router.get('/:id', async (req: { params: { id: string; }; }, res: { send: (arg0: IUser) => void; }) => {
+  const model = new UserModel('User', userSchema);
   const user = await model.byId(req.params.id);
   res.send(user)
 })
